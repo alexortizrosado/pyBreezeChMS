9383bb801c7a83f2f6c037fdd9b1aa6ec1faec00
Alex Ortiz-Rosado <aortiz32@gmail.com>
2014-05-07 23:41:29 -0700

    Add an all_tests test suite

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+import unittest
+
+from breeze_test import BreezeApiTestCase
+
+
+def all_tests():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(BreezeApiTestCase))
+    return suite
