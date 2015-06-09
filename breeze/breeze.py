66f8108cfd2d239609331fe8a8f5577b23a8e2cf
Alex Ortiz-Rosado <aortiz32@gmail.com>
2015-06-09 11:15:38 -0700

    Add useful pydoc and reformat method names.

--- breeze/breeze.py
+++ breeze/breeze.py
@@ -1,3 +1,22 @@
+"""Python wrapper for Breeze ChMS API: http://breezechms.com/docs#extensions_api
+
+The Breeze API allows churches to build custom functionality integrated with
+Breeze.
+
+Usage:
+  from breeze import breeze
+  
+  breeze_api = breeze.BreezeApi(
+      breeze_url='https://demo.breezechms.com',
+      api_key='5c2d2cbacg3...')
+  people = breeze_api.get_people();
+
+  for person in people:
+    print '%s %s' % (person['first_name'], person['last_name'])
+"""
+
+__author__ = 'alex@rohichurch.org (Alex Ortiz-Rosado)'
+
 import requests
 
 from utils import make_enum
@@ -13,8 +32,22 @@ class BreezeError(Exception):
 
 
 class BreezeApi(object):
+  """A wrapper for the Breeze REST API."""
 
-  def __init__(self, breeze_url, api_key, debug=False, dry_run=False, connection=requests.Session()):
+  def __init__(self, breeze_url, api_key, debug=False, dry_run=False,
+              connection=requests.Session()):
+  """Instantiates the BreezeApi with your Breeze account information.
+
+  Args:
+    breeze_url: Fully qualified domain for your organizations Breeze service.
+    api_key: Unique Breeze API key. For instructions on finding your
+             organizations API key, see:
+             http://breezechms.com/docs#extensions_api
+    debug: Enable debug output.
+    dry_run: Enable no-op mode, which disables requests from being made. When
+             combined with debug, this allows debugging requests without
+             affecting data in your Breeze account.
+  """
     self.breeze_url = breeze_url
     self.api_key = api_key
     self.debug = debug
@@ -31,7 +64,21 @@ class BreezeApi(object):
     if not self.api_key:
       raise BreezeError('You must provide an API key.')
 
-  def _request(self, endpoint, params=None, headers=None, timeout=60):
+  def _Request(self, endpoint, params=None, headers=None, timeout=60):
+    """Makes an HTTP request to a given url.
+
+    Args:
+      endpoint: URL where the service can be accessed.
+      params: Query parameters to append to endpoint url.
+      headers: HTTP headers; used for authenication parameters.
+      timeout: Timeout in seconds for HTTP request.
+
+    Returns:
+      HTTP response
+
+    Throws:
+      BreezeError if connection or request fails.
+    """
     headers = {'Content-Type': 'application/json',
                'Api-Key': self.api_key}
     
@@ -55,31 +102,76 @@ class BreezeApi(object):
             raise BreezeError(response)
         return response
   
-  def _request_succeeded(self, response):
+  def _RequestSucceeded(self, response):
+    """Predicate to ensure that the HTTP request succeeded."""
     return not (('error' in response) or ('errorCode' in response))
 
   
-  def get_people(self, limit=None, start=None, details=False):
+  def GetPeople(self, limit=None, offset=None, details=False):
+    """List people from your database.
+
+    Args:
+      limit: Number of people to return. If None, will return all people.
+      offset: Number of people to skip before beginning to return results.
+              Can be used in conjunction with limit for pagination.
+      details: Option to return all information (slower) or just names.
+
+    returns:
+      JSON response. For example:
+      {
+        "id":"157857",
+        "first_name":"Thomas",
+        "last_name":"Anderson",
+        "path":"img\/profiles\/generic\/blue.jpg"
+      },
+      {
+        "id":"157859",
+        "first_name":"Kate",
+        "last_name":"Austen",
+        "path":"img\/profiles\/upload\/2498d7f78s.jpg"
+      },
+      {
+        ...
+      }
+    """
+
     params = []
     if limit:
       params.append('limit=%s' % limit)
     if start:
-      params.append('start=%s' % start)
+      params.append('offset=%s' % start)
     if details:
       params.append('details=1')
-    return self._request('%s/?%s' % (ENDPOINTS.PEOPLE, '&'.join(params)))
+    return self._Request('%s/?%s' % (ENDPOINTS.PEOPLE, '&'.join(params)))
 
   
-  def get_profile_fields(self):
+  def GetProfileFields(self):
+    """List profile fields from your database.
+
+    Returns:
+      JSON response.
+    """
     return self._request(ENDPOINTS.PROFILE_FIELDS)
   
-  def get_person_details(self, person_id):
+  def GetPersonDetails(self, person_id):
+    """Retrieve the details for a specific person by their ID.
+
+    Args:
+      person_id: Unique id for a person in Breeze database.
+
+    Returns:
+      JSON response.
+    """
     return self._request('%s/%s' % (ENDPOINTS.PEOPLE, str(person_id)))
   
-  def get_events(self, start_date=None, end_date=None):
-    """
-    Retrieve events in given date.
-    defaults to current month if no parameters supplied.
+  def GetEvents(self, start_date=None, end_date=None):
+    """Retrieve all events for a given date range.
+    Args:
+      start_date: Start date; defaults to first day of the current month.
+      end_date: End date; defaults to last day of the current month
+
+    Returns:
+      JSON response.
     """
     params = []
     if start_date:
@@ -88,78 +180,114 @@ class BreezeApi(object):
       params.append('end=%s' % end_date)
     return self._request('%s/?%s' % (ENDPOINTS.EVENTS, '&'.join(params)))
   
-  def event_check_in(self, person_id, event_instance_id):
-    """
-    Check In Person
-     /api/events/attendance/add?person_id=[PERSONID]&instance_id=[INSTANCEID]
+  def EventCheckIn(self, person_id, event_instance_id):
+    """Checks in a person into an event.
+
+    Args:
+      person_id: id for a person in Breeze database.
+      event_instance_id: id for event instance to check into.
+
+    Returns:
+      True if check-in succeeds; False if check-in fails.
     """
     return self._request('%s/attendance/add?person_id=%s&instance_id=%s' % (
         ENDPOINTS.EVENTS, str(person_id), str(event_instance_id)))
     
   
-  def event_check_out(self, person_id, event_instance_id):
-    """
-    Remove Checked In Person
-     /api/events/attendance/delete?person_id=[PERSONID]&instance_id=[INSTANCEID]
+  def EventCheckOut(self, person_id, event_instance_id):
+    """Remove the attendance for a person checked into an event.
+
+    Args:
+      person_id: Breeze ID for a person in Breeze database.
+      event_instance_id: id for event instance to check out (delete).
+
+    Returns:
+      True if check-in succeeds; False if check-in fails.
     """
     return self._request(
         '%s/attendance/delete?person_id=%s&instance_id=%s' % (
             ENDPOINTS.EVENTS, str(person_id), str(event_instance_id)))
 
-  def add_contribution(self, date=None, name=None, person_id=None, uid=None,
-                       processor=None, method=None, funds_json=None,
-                       amount=None, group=None, batch_number=None,
-                       batch_name=None):
-    """
-    api/giving/add?
-      date=[DATE]&
-      name=[NAME]&
-      person_id=[PERSON_ID]&
-      uid=[UID]&
-      processor=[PROCESSOR]&
-      method=[METHOD]&
-      funds_json=[FUNDS_JSON]&
-      amount=[AMOUNT]&
-      group=[GROUP]&
-      batch_number=[BATCH NUMBER]&
-      batch_name=[BATCH_NAME]
+  def AddContribution(self, date=None, name=None, person_id=None, uid=None,
+                      processor=None, method=None, funds_json=None,
+                      amount=None, group=None, batch_number=None,
+                      batch_name=None):
+    """Add a contribution to Breeze.
+
+    Args:
+      date: Date of transaction in DD-MM-YYYY format (ie. 24-5-2015)
+      name: Name of person that made the transaction. Used to help match up
+            contribution to correct profile within Breeze.  (ie. John Doe)
+      person_id: The Breeze ID of the donor. If unknown, use UID instead of
+                 person id  (ie. 1234567)
+      uid: The unique id of the person sent from the giving platform. This
+           should be used when the Breeze ID is unknown. Within Breeze a user
+           will be able to associate this ID with a given Breeze ID.
+           (ie. 9876543)
+      email: Email address of donor. If no person_id is provided, used to help
+             automatically match the person to the correct profile.
+             (ie. sample@breezechms.com)
+      street_address: Donor's street address. If person_id is not provided,
+                      street_address will be used to help automatically match
+                      the person to the correct profile.  (ie. 123 Sample St)
+      processor: The name of the processor used to send the payment. Used in
+                 conjunction with uid. Not needed if using Breeze ID.
+                 (ie. SimpleGive, BluePay, Stripe)
+      method: The payment method. (ie. Check, Cash, Credit/Debit Online, 
+              Credit/Debit Offline, Donated Goods (FMV), Stocks (FMV), 
+              Direct Deposit)
+      funds_json: JSON string containing fund names and amounts. This allows
+                  splitting fund giving. The ID is optional. If present, it must
+                  match an existing fund ID and it will override the fund name.
+                  ie. [ {
+                          'id':'12345',
+                          'name':'General Fund',
+                          'amount':'100.00'
+                        },
+                        {
+                          'name':'Missions Fund',
+                          'amount':'150.00'
+                        }
+                      ]
+      amount: Total amount given. Must match sum of amount in funds_json.
+      group: This will create a new batch and enter all contributions with the
+             same group into the new batch. Previous groups will be remembered
+             and so they should be unique for every new batch. Use this if
+             wanting to import into the next batch number in a series.
+      batch_number: The batch number to import contributions into. Use group
+                    instead if you want to import into the next batch number.
+      batch_name: The name of the batch. Can be used with batch number or group.
+
+    Returns:
+      
     """
     params = []
     if date:
       params.append('date=%s' % date)
-
     if name:
       params.append('name=%s' % name)
-
     if person_id:
       params.append('person_id=%s' % person_id)
-
     if uid:
       params.append('uid=%s' % uid)
-
     if processor:
       params.append('processor=%s' % processor)
-
     if method:
       params.append('method=%s' % method)
-
     if funds_json:
       params.append('funds_json=%s' % funds_json)
-
     if amount:
       params.append('amount=%s' % amount)
-
     if group:
       params.append('group=%s' % group)
-
     if batch_number:
       params.append('batch_number=%s' % batch_number)
-
     if batch_name:
       params.append('batch_name=%s' % dabatch_namete)
-    response = self._request('%s/add?%s' % (ENDPOINTS.CONTRIBUTIONS, '&'.join(params)))
+    response = self._request('%s/add?%s' % (ENDPOINTS.CONTRIBUTIONS,
+                                            '&'.join(params)))
     if not response['success']:
-      self.logger.error(response['errors'])
+      raise BreezeError('Failed to add contribution: ', response['errors'])
 
 
 
--- tests/breeze_test.py
+++ tests/breeze_test.py
@@ -1,3 +1,5 @@
+"""Unittests for breeze.py"""
+
 import json
 import unittest
 
@@ -6,6 +8,7 @@ from breeze.breeze import BreezeError
 
 
 class MockConnection(object):
+  """Mock requests connection."""
   def __init__(self, response):
     self.response = response
     self.headers = {}
@@ -14,6 +17,7 @@ class MockConnection(object):
     return self.response
 
 class MockResponse(object):
+  """ Mock requests HTTP response."""
   def __init__(self, status_code, content):
     self.status_code = status_code
     self.content = content
@@ -36,7 +40,7 @@ class BreezeApiTestCase(unittest.TestCase):
     self.FAKE_API_KEY = 'fak3ap1k3y'
     self.FAKE_SUBDOMAIN = 'https://demo.breezechms.com'
   
-  def test_invalid_subdomain(self):
+  def testInvalidSubdomain(self):
     self.assertRaises(BreezeError,
         lambda: BreezeApi(api_key=self.FAKE_API_KEY, breeze_url='invalid-subdomain'))
     self.assertRaises(BreezeError,
@@ -44,67 +48,67 @@ class BreezeApiTestCase(unittest.TestCase):
     self.assertRaises(BreezeError,
         lambda: BreezeApi(api_key=self.FAKE_API_KEY, breeze_url=''))
   
-  def test_missing_api_key(self):
+  def testMissingApiKey(self):
     self.assertRaises(BreezeError,
         lambda: BreezeApi(api_key=None, breeze_url=self.FAKE_SUBDOMAIN))
     self.assertRaises(BreezeError,
         lambda: BreezeApi(api_key='', breeze_url=self.FAKE_SUBDOMAIN))
   
-  def test_get_people(self):
+  def testGetPeople(self):
     response = MockResponse(200, json.dumps({'name': 'Some Data.'}))
     connection = MockConnection(response)
     breeze = BreezeApi(
         breeze_url=self.FAKE_SUBDOMAIN,
         api_key=self.FAKE_API_KEY,
         connection=connection)
-    self.assertEqual(breeze.get_people(), json.loads(response.content))
+    self.assertEqual(breeze.GetPeople(), json.loads(response.content))
   
-  def test_get_profile_fields(self):
+  def testGetProfileFields(self):
     response = MockResponse(200, json.dumps({'name': 'Some Data.'}))
     connection = MockConnection(response)
     breeze = BreezeApi(
         breeze_url=self.FAKE_SUBDOMAIN,
         api_key=self.FAKE_API_KEY,
         connection=connection)
-    self.assertEqual(breeze.get_profile_fields(), json.loads(response.content))
+    self.assertEqual(breeze.GetProfileFields(), json.loads(response.content))
   
-  def test_get_person_details(self):
+  def testGetPersonDetails(self):
     response = MockResponse(200, json.dumps({'person_id': 'Some Data.'}))
     connection = MockConnection(response)
     breeze = BreezeApi(
         breeze_url=self.FAKE_SUBDOMAIN,
         api_key=self.FAKE_API_KEY,
         connection=connection)
-    self.assertEqual(breeze.get_person_details('person_id'),
+    self.assertEqual(breeze.GetPersonDetails('person_id'),
                      json.loads(response.content))
   
-  def test_get_events(self):
+  def testGetEvents(self):
     response = MockResponse(200, json.dumps({'event_id': 'Some Data.'}))
     connection = MockConnection(response)
     breeze = BreezeApi(
         breeze_url=self.FAKE_SUBDOMAIN,
         api_key=self.FAKE_API_KEY,
         connection=connection)
-    self.assertEqual(breeze.get_events(),json.loads(response.content))
+    self.assertEqual(breeze.GetEvents(),json.loads(response.content))
   
-  def test_event_check_in(self):
+  def testEventCheckIn(self):
     response = MockResponse(200, json.dumps({'event_id': 'Some Data.'}))
     connection = MockConnection(response)
     breeze = BreezeApi(
         breeze_url=self.FAKE_SUBDOMAIN,
         api_key=self.FAKE_API_KEY,
         connection=connection)
-    self.assertEqual(breeze.event_check_in('person_id', 'event_id'),
+    self.assertEqual(breeze.EventCheckIn('person_id', 'event_id'),
                      json.loads(response.content))
   
-  def test_event_check_out(self):
+  def testEventCheckOut(self):
     response = MockResponse(200, json.dumps({'event_id': 'Some Data.'}))
     connection = MockConnection(response)
     breeze = BreezeApi(
         breeze_url=self.FAKE_SUBDOMAIN,
         api_key=self.FAKE_API_KEY,
         connection=connection)
-    self.assertEqual(breeze.event_check_out('person_id', 'event_id'),
+    self.assertEqual(breeze.EventCheckOut('person_id', 'event_id'),
                      json.loads(response.content))
 
 
