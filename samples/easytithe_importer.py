19097000d1fd57c76e633e9f62117c69b427ba0d
Alex Ortiz-Rosado <alexortizrosado@gmail.com>
2018-04-14 23:59:53 -0700

    Update easytithe submodule

--- samples/easytithe_importer.py
+++ samples/easytithe_importer.py
@@ -58,7 +58,7 @@ class Contribution(object):
     @property
     def date(self):
         formatted_date = datetime.strptime(
-            self._contribution['Date'], '%m/%d/%Y %I:%M:%S %p')
+            self._contribution['Date'], '%m/%d/%Y')
         return formatted_date.strftime('%Y-%m-%d')
 
     @property
