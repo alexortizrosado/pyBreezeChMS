2d76dfbb681d8d17da41c2ff16b722e393b01b56
Alex Ortiz-Rosado <alex@breezechms.com>
2020-06-11 08:59:11 -0700

    Add tests for update_person_with_fields_json

--- tests/breeze_test.py
+++ tests/breeze_test.py
@@ -188,6 +188,35 @@ class BreezeApiTestCase(unittest.TestCase):
         )
         self.assertEqual(breeze_api.update_person(person_id, '[]'),
                          json.loads(response.content))
+    
+    def test_update_person_with_fields_json(self):
+        response = MockResponse(200, json.dumps([{'person_id': 'Some Data.'}]))
+        connection = MockConnection(response)
+        breeze_api = breeze.BreezeApi(
+            breeze_url=FAKE_SUBDOMAIN,
+            api_key=FAKE_API_KEY,
+            connection=connection)
+
+        person_id = '123456'
+        fields_json = json.dumps([{
+            "field_id": "929778337",
+            "field_type": "email",
+            "response": "true",
+            "details": {
+                 "address": "tony@starkindustries.com",
+                 "is_private": 1
+            }
+        }], separators=(',', ':'))
+        breeze_api.update_person(person_id, fields_json)
+        self.assertEqual(
+            connection.url, '%s%s/update?%s' %
+                            (FAKE_SUBDOMAIN, breeze.ENDPOINTS.PEOPLE, '&' . join(
+                                ['person_id=%s' % person_id,
+                                 'fields_json=%s' % fields_json])
+                             )
+        )
+        self.assertEqual(breeze_api.update_person(person_id, json.dumps(fields_json, separators=(',', ':'))),
+                         json.loads(response.content))
 
     def test_get_events(self):
         response = MockResponse(200, json.dumps({'event_id': 'Some Data.'}))
@@ -478,9 +507,6 @@ class BreezeApiTestCase(unittest.TestCase):
             connection.url,
             '%s%s/list_pledges?campaign_id=329' % (FAKE_SUBDOMAIN,
                                                    breeze.ENDPOINTS.PLEDGES))
-
-    def test_get_tag_folders(self):
-        pass
 
     def test_get_tags(self):
         response = MockResponse(200, json.dumps([{
